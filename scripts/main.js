e9db1efec64db7f14c1ad94948b2ac235de6a52e
Max_Moses <maxmoses11@gmail.com>
2025-05-03 22:40:07 +0300

    input mask to phone input added

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VidePlayer.js";
 import ExpandableContentCollection from "./ExpandableContent.js";
+import InputMaskCollection from "./InputMask.js";
 
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
-new ExpandableContentCollection()
+new ExpandableContentCollection();
+new InputMaskCollection();
